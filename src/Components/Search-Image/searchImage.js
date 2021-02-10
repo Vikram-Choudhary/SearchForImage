ea46e0a362f548e00589b89bc353605dfc9c9f74
Vikram-Choudhary <vikramus4@gmail.com>
2021-02-11 00:50:08 +0530

    Updated to deploy

--- src/Components/Search-Image/searchImage.js
+++ src/Components/Search-Image/searchImage.js
@@ -2,7 +2,20 @@ import React, { useState, useEffect, useRef } from "react";
 const imageList = {
   beach: {
     folderName: "Beach",
-    imgList: ["Beach1", "Beach2", "Beach3", "Beach4", "Beach5", "Beach6"],
+    imgList: [
+      "Beach1",
+      "Beach2",
+      "Beach3",
+      "Beach4",
+      "Beach5",
+      "Beach6",
+      "Beach7",
+      "Beach8",
+      "Beach9",
+      "Beach10",
+      "Beach11",
+      "Beach12",
+    ],
   },
   bird: {
     folderName: "Bird",
@@ -15,11 +28,28 @@ const imageList = {
       "Bird6",
       "Bird7",
       "Bird8",
+      "Bird9",
+      "Bird10",
+      "Bird11",
+      "Bird12",
     ],
   },
   food: {
     folderName: "Food",
-    imgList: ["Food1", "Food2", "Food3", "Food4", "Food5", "Food6", "Food7"],
+    imgList: [
+      "Food1",
+      "Food2",
+      "Food3",
+      "Food4",
+      "Food5",
+      "Food6",
+      "Food7",
+      "Food8",
+      "Food9",
+      "Food10",
+      "Food11",
+      "Food12",
+    ],
   },
   mountain: {
     folderName: "Mountain",
@@ -32,6 +62,10 @@ const imageList = {
       "mountain6",
       "mountain7",
       "mountain8",
+      "mountain9",
+      "mountain10",
+      "mountain11",
+      "mountain12",
     ],
   },
   "": {},
@@ -52,7 +86,7 @@ const ImageCard = (props) => {
         ref={imgRef}
         alt={`${props.folderName}-->${props.imgSrc}`}
         src={require(`../../assets/images/${props.folderName}/${props.imgSrc}.jpg`)}
-      />
+      />{" "}
     </div>
   );
 };
@@ -62,6 +96,7 @@ export default function SearchImage(props) {
       const { folderName, imgList = [] } = imageList[props.searchedValue];
       return (
         <div className="imgList">
+          {" "}
           {imgList.map((imgName, index) => {
             try {
               return (
@@ -74,11 +109,11 @@ export default function SearchImage(props) {
             } catch (error) {
               return null;
             }
-          })}
+          })}{" "}
         </div>
       );
     } catch (error) {
-      return <div className="noData">No Image Found...</div>;
+      return <div className="noData"> No Image Found... </div>;
     }
   };
   return heplerFunction();
